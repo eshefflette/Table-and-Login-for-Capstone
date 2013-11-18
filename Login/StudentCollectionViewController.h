9e31f307ef334383fcee1c054bfd4d129095aea3
Eric Svendsen Shefflette <esvendsenshefflette@csumb.edu>
2013-11-17 19:32:33 -0800

    Added collection view

---
+++ Login/StudentCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  StudentCollectionViewController.h
+//  Login
+//
+//  Created by Eric Shefflette on 11/17/13.
+//  Copyright (c) 2013 Eric Svendsen Shefflette. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StudentCollectionViewController : UICollectionViewController
+
+@end
